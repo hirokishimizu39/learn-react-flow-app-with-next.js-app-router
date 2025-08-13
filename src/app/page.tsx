1eb913a64674229b3c6773d1fb7804b7f18df1f5
Hiroki Shimizu <hiroki71027923@icloud.com>
2025-08-13 18:37:45 +0900

    reactflowの最も基本的な形がこれ。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,56 @@
+"use client";
+
 import "@xyflow/react/dist/style.css";
-import { ReactFlow, Background, Controls } from "@xyflow/react";
+import { ReactFlow, Background, Controls, applyNodeChanges, NodeChange, applyEdgeChanges, addEdge } from "@xyflow/react";
+import { useCallback, useState } from "react";
+
+const initialNodes = [
+  {
+    id: "1",
+    position: { x: 0, y: 0 },
+    type: "input",
+    data: { label: "1" },
+  },
+  {
+    id: "2",
+    position: { x: 0, y: 100 },
+    type: "input",
+    data: { label: "2" },
+  },
+];
+
+const initialEdges = [
+  { id: 'e1-2', source: '1', target: '2' },
+];
 
 export default function HomePage() {
+
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    [],
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    [],
+  );
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [],
+  );
+
   return (
-    <div className="relative size-full">
-      <div className="absolute top-0 left-0 size-full text-yellow-500">
-        reactflowを実装するまでの表示用
-      </div>
-      <ReactFlow>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
         <Controls />
         <Background />
       </ReactFlow>
