20307728ebddf8031642aabfa0dce8a6a835ff17
Hiroki Shimizu <hiroki71027923@icloud.com>
2025-08-13 17:23:00 +0900

    react flowの実装直前まで。　これから、node, edgeの定義。定義した値を<ReactFlow>で過去って、マインドマップを表示する

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
-export default function Page() {
-  return <div>Hello World</div>;
+import "@xyflow/react/dist/style.css";
+import { ReactFlow, Background, Controls } from "@xyflow/react";
+
+export default function HomePage() {
+  return (
+    <div className="relative size-full">
+      <div className="absolute top-0 left-0 size-full text-yellow-500">
+        reactflowを実装するまでの表示用
+      </div>
+      <ReactFlow>
+        <Controls />
+        <Background />
+      </ReactFlow>
+    </div>
+  );
 }
